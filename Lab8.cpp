f4530f489dd012c10166f1cc0edf139ca31045b5
Violetta01p <pvsergeevnaa@gmail.com>
2024-11-12 20:23:50 +0200

    Create Lab8.cpp

---
+++ Lab8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+ using namespace std;
+ int main () {
+string input;
+cout << "Enter text: "; 
+cin >> input;
+reverse (input.begin(), input.end ());
+cout < <"reserve text: " << input << endl;
+return 0;
+｝
